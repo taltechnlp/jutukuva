95cf9bda2520b70b242b611c691e3e2469d723d0
Aivo Olev <aivo.olev@gmail.com>
2025-12-04 10:06:36 +0200

    Fix broken sessions link in collaboration dropdown
    Replace href="/sessions" with button that opens SessionsModal,
    since the sessions route was removed.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1470,12 +1470,12 @@
 
 							<div class="divider my-2"></div>
 							<li>
-								<a href="/sessions" class="gap-3">
+								<button class="gap-3 cursor-pointer" onclick={() => modalStore.openSessions()}>
 									<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
 									</svg>
 									{$_('collaboration.manage_sessions')}
-								</a>
+								</button>
 							</li>
 						{:else}
 							<li><h3 class="menu-title text-sm font-semibold">{$_('collaboration.session_active')}</h3></li>
