0ed7981367594a3458bd9909e961ad7a1d66d41c
Aivo Olev <aivo.olev@gmail.com>
2025-12-09 15:19:51 +0200

    Remove electrobun-viewer package
    Replaced by overlay-captions Tauri 2 package which provides better
    window management and cross-platform support.

--- packages/electrobun-viewer/src/views/settings/script.js
+++
@@ -1,9 +0,0 @@
-/**
- * Settings View Script
- * Minimal script for standalone settings page
- */
-
-console.log('[Settings] View loaded');
-
-// This view is primarily informational
-// Most settings are handled in the main view's modal
