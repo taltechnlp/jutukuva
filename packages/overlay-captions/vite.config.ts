fc69f345800d6b89b3b3fd0922698b3e7ff158b4
Bohdan <pb0961713194@gmail.com>
2025-12-11 13:48:25 +0200

    initial

--- packages/overlay-captions/vite.config.ts
+++ packages/overlay-captions/vite.config.ts
@@ -1,25 +1,28 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	clearScreen: false,
-	server: {
-		port: 1420,
-		strictPort: true,
-		host: host || false,
-		hmr: host
-			? {
-					protocol: 'ws',
-					host,
-					port: 1421
-				}
-			: undefined,
-		watch: {
-			ignored: ['**/src-tauri/**']
-		}
-	}
+  plugins: [tailwindcss(), sveltekit()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+    fs: {
+      allow: ["."],
+    },
+  },
 });
