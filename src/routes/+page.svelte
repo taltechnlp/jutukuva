ed62b4d94bcd843de2fa2957a655217e9a99afd2
Aivo Olev <aivo.olev@gmail.com>
2025-12-19 12:35:35 +0200

    Allow guests to use ASR recording in collaborative sessions
    Remove the restriction that disabled ASR recording when joining
    another person's session. This was originally set to avoid confusion
    but is needed when the other person is unavailable.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1432,8 +1432,8 @@
 					<button
 						class="btn btn-circle btn-primary btn-lg shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-200"
 						onclick={startRecording}
-						disabled={!isWasmReady || (sessionInfo?.role === 'guest')}
-						title={sessionInfo?.role === 'guest' ? $_('collaboration.guest_cannot_record') : $_('dictate.startRecording')}
+						disabled={!isWasmReady}
+						title={$_('dictate.startRecording')}
 					>
 						<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
